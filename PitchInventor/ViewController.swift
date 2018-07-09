2ab3996bf9933be1e0db0b2978eb87e6dc554949
Sakura Rapolu <rapolusakura@gmail.com>
2018-07-09 09:55:40 -0700

    added completed

--- PitchInventor/ViewController.swift
+++ PitchInventor/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var labelOne: [String] = ["Make School", "Uber", "Netflix", "Facebook", "Google", "Kickstarter", "Spotify", "Airbnb", "Snapchat", "YouTube", "Dropbox", "Amazon", "Craigslist", "Tinder", "Instagram", "Tesla", "Twitter", "SpaceX"]
+    var labelTwo: [String] = ["Dogs", "Books", "Gamers", "Star Wars", "Bitcoin", "Goats", "Zombies", "Rich People", "Swimmers", "Florida", "Vampires", "Dragons", "Internet of Things", "Mars", "Cryptocurrency", "Mosquito Repellent", "Fidget Spinners", "Sun Screen", "Water Bottles", "Lost Travelers", "Superheroes"]
+    
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    
+    @IBAction func newButton(_ sender: Any) {
+        label1.text = labelOne[Int(arc4random_uniform(UInt32(labelOne.count)))]
+        label2.text = labelTwo[Int(arc4random_uniform(UInt32(labelTwo.count)))]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
